1183b4e3be2676147018eaefd002fb6f9d77b750
Michael Pablo Gomes da Silva <MichaelPablo13@users.noreply.github.com>
2023-04-09 02:04:30 -0300

    Update backend.tf
    name change bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "michael-vorx-shadow-terraform-1"
+    bucket = "michael-vorx-shadow-terraform"
     key    = "vorx-network-shadow.tfstate"
     region = "us-east-1"
   }
